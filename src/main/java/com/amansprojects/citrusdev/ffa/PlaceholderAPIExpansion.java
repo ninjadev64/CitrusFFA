e23fe3bc11444efc517ea3acc4be893907fb4988
ninjadev64 <63245705+ninjadev64@users.noreply.github.com>
2022-04-01 19:55:35 +0100

    Add KDR placeholder, use a StatisticCase for kills and deaths placeholders and fix an IntelliJ warning

--- src/main/java/com/amansprojects/citrusdev/ffa/PlaceholderAPIExpansion.java
+++ src/main/java/com/amansprojects/citrusdev/ffa/PlaceholderAPIExpansion.java
@@ -5,6 +5,7 @@ import org.bukkit.OfflinePlayer;
 
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -15,31 +16,29 @@ public class PlaceholderAPIExpansion extends PlaceholderExpansion {
 
 	public PlaceholderAPIExpansion(Main main) { this.plugin = main; }
 
-	@Override public String getAuthor() { return "ninjadev64 (IGN ninjagamer64)"; }
+	@Override @NotNull public String getAuthor() { return "ninjadev64 (IGN ninjagamer64)"; }
     
-	@Override public String getIdentifier() { return "citrusffa"; }
+	@Override @NotNull public String getIdentifier() { return "citrusffa"; }
 
-    @Override public String getVersion() { return this.plugin.getDescription().getVersion(); }
+    @Override @NotNull public String getVersion() { return this.plugin.getDescription().getVersion(); }
     
     @Override public boolean persist() { return true; }
     
     @Override
     public String onRequest(OfflinePlayer player, String params) {
-        if (params.equalsIgnoreCase("kills")) {
-        	int kills = this.plugin.statConfigManager.getConfig().getInt(player.getUniqueId() + ".kills");
-        	if (kills == 0) { return "0"; }
-        	else { return Integer.toString(kills); }
-        }
-        
-        if (params.equalsIgnoreCase("deaths")) {
-        	int deaths = this.plugin.statConfigManager.getConfig().getInt(player.getUniqueId() + ".deaths");
-        	if (deaths == 0) { return "0"; }
-        	else { return Integer.toString(deaths); }
-        }
+        UUID uuid = player.getUniqueId();
+        FileConfiguration statsConfig = this.plugin.statConfigManager.getConfig();
+        StatisticCase stats = new StatisticCase(
+                uuid.toString(),
+                statsConfig.getInt(uuid + ".kills"),
+                statsConfig.getInt(uuid + ".deaths")
+        );
 
-        if (params.startsWith("leaderboard_")) {
+        if (params.equalsIgnoreCase("kills")) { return Integer.toString(stats.KILLS); }
+        else if (params.equalsIgnoreCase("deaths")) { return Integer.toString(stats.DEATHS); }
+
+        else if (params.startsWith("leaderboard_")) {
             try {
-                FileConfiguration statsConfig = this.plugin.statConfigManager.getConfig();
                 ArrayList<StatisticCase> values = new ArrayList<StatisticCase>();
                 for (String path : statsConfig.getKeys(false)) {
                     values.add(new StatisticCase(path, statsConfig.getInt(path + ".kills"), statsConfig.getInt(path + ".deaths")));
@@ -55,6 +54,11 @@ public class PlaceholderAPIExpansion extends PlaceholderExpansion {
             } catch (ArrayIndexOutOfBoundsException | NullPointerException e) {
                 return null;
             }
+        }
+
+        else if (params.equalsIgnoreCase("kdr")) {
+            try { return String.valueOf(Math.round(((float)stats.KILLS / stats.DEATHS) * 100.0) / 100.0); }
+            catch (ArithmeticException e) { return "Infinity"; }
         }
 
         return null;
